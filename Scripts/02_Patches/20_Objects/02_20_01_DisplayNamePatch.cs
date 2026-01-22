c44a9c6309e54889e1537d21cbb646da6f8a5d16
boram <boramkim@knou.ac.kr>
2026-01-22 14:24:52 +0900

    chore: quick save - 2026-01-22 14:24:52

--- Scripts/02_Patches/20_Objects/02_20_01_DisplayNamePatch.cs
+++ Scripts/02_Patches/20_Objects/02_20_01_DisplayNamePatch.cs
@@ -4,6 +4,7 @@
  * 역할: GetDisplayNameEvent.GetFor() 패치로 생물/아이템 이름 한글화
  * 작성일: 2026-01-22
  * 비고: ForSort, ColorOnly 모드에서는 번역 스킵
+ * 수정: 2026-01-22 - Harmony 패치 시그니처 수정 (파라미터 타입 명시)
  */
 
 using System;
@@ -19,6 +20,25 @@ namespace QudKorean.Objects
     /// Translates creature and item display names to Korean.
     /// </summary>
     [HarmonyPatch(typeof(GetDisplayNameEvent))]
+    [HarmonyPatch("GetFor")]
+    [HarmonyPatch(new Type[] { 
+        typeof(GameObject),  // Object
+        typeof(string),      // Base
+        typeof(int),         // Cutoff
+        typeof(string),      // Context
+        typeof(bool),        // AsIfKnown
+        typeof(bool),        // Single
+        typeof(bool),        // NoConfusion
+        typeof(bool),        // NoColor
+        typeof(bool),        // ColorOnly
+        typeof(bool),        // Visible
+        typeof(bool),        // BaseOnly
+        typeof(bool),        // UsingAdjunctNoun
+        typeof(bool),        // WithoutTitles
+        typeof(bool),        // ForSort
+        typeof(bool),        // Reference
+        typeof(bool)         // IncludeImplantPrefix
+    })]
     public static class Patch_ObjectDisplayName
     {
         private const string LOG_PREFIX = "[QudKR-Objects]";
@@ -27,12 +47,10 @@ namespace QudKorean.Objects
         /// Postfix patch for GetDisplayNameEvent.GetFor().
         /// CRITICAL: Must check ForSort and ColorOnly parameters to avoid breaking sorting/color-only operations.
         /// </summary>
-        [HarmonyPatch(nameof(GetDisplayNameEvent.GetFor))]
         [HarmonyPostfix]
         static void GetFor_Postfix(
             ref string __result,
             GameObject Object,
-            string Base,
             bool ColorOnly,    // If true, only returns color code - DO NOT translate
             bool ForSort)      // If true, used for sorting - DO NOT translate
         {
